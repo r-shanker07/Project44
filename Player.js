e80c0600eda04a4bcd40a111133e3a49a2f8e050
r-shanker07 <68451701+r-shanker07@users.noreply.github.com>
2020-10-28 21:31:13 -0400

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,11 @@
+class Player extends BaseClass{
+    constructor(x,y){
+        super(x,y,50,50);
+        this.image = loadImage("images/player.png");
+    }
+
+    display(){
+
+        super.display();
+    }
+}
